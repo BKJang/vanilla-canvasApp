8db293904f6ed87177bc6dea8f8b9bd8e94c5115
BKJang <jp302119@gmail.com>
2019-06-09 00:54:08 +0900

    :banana: canvas context strokeStyle 변경 기능 구현

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
 //css 사이즈가 아닌 canvas 자체적으로 width와 height를 줘야 동작한다.(pixel modifier)
 canvas.width = 470;
 canvas.height = 470;
 
 
-ctx.strokStyle = '#2c2c2c';
+ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -17,6 +18,7 @@ function stopPainting() {
     painting = false;
 }
 
+// 캔버스에 마우스를 클릭했을 때 이벤트
 function startPainting() {
     painting = true;
 }
@@ -27,7 +29,7 @@ function onMouseMove(event) {
     const y = event.offsetY;
 
     //움직이면서 path를 만듬
-    if(!painting) {
+    if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
@@ -37,16 +39,23 @@ function onMouseMove(event) {
     }
 }
 
-// 캔버스에 마우스를 클릭했을 때 이벤트
-function onMouseDown(event) {
-    //마우스를 클릭했을 때  painting flag를 true로 변경
-    startPainting();
+function handleChangeColor(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
-// 마우스가 캔버스 내부에서 인지할 수 있도록 이벤트 리스너 추가
-if(canvas) {
-    canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', startPainting);
-    canvas.addEventListener('mouseup', stopPainting);
-    canvas.addEventListener('mouseleave', stopPainting);
+function init() {
+    // 마우스가 캔버스 내부에서 인지할 수 있도록 이벤트 리스너 추가
+    if (canvas) {
+        canvas.addEventListener('mousemove', onMouseMove);
+        canvas.addEventListener('mousedown', startPainting);
+        canvas.addEventListener('mouseup', stopPainting);
+        canvas.addEventListener('mouseleave', stopPainting);
+    }
+
+    [...colors].map((color) => {
+        color.addEventListener('click', handleChangeColor);
+    })
 }
+
+init();
