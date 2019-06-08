998abd2b1a745e2e36cffe6f7658ad2ceea947d5
BKJang <jp302119@gmail.com>
2019-06-09 00:44:08 +0900

    :banana: canvas context 2d 적용

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+//css 사이즈가 아닌 canvas 자체적으로 width와 height를 줘야 동작한다.(pixel modifier)
+canvas.width = 470;
+canvas.height = 470;
+
+
+ctx.strokStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
+// 캔버스에서 마우스가 벗어났을 때 이벤트
+// 캔버스에서 마우스를 클릭했다가 뗐을 때 이벤트
 function stopPainting() {
     painting = false;
 }
@@ -14,6 +25,16 @@ function startPainting() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+
+    //움직이면서 path를 만듬
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        //마우스를 클릭했을 때 그리기 시작
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 // 캔버스에 마우스를 클릭했을 때 이벤트
@@ -22,22 +43,10 @@ function onMouseDown(event) {
     startPainting();
 }
 
-// 캔버스에서 마우스를 클릭했다가 뗐을 때 이벤트
-function onMouseUp(event) {
-    //마우스를 클릭했다가 띠면 painting flag를 false로 변경
-    stopPainting();
-}
-
-// 캔버스에서 마우스가 벗어났을 때 이벤트
-function onMouseLeave(event) {
-    //마우스가 캔버스에서 벗어나면 painting flag를 false로 변경
-    stopPainting();
-}
-
 // 마우스가 캔버스 내부에서 인지할 수 있도록 이벤트 리스너 추가
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
-    canvas.addEventListener('mouseleave', onMouseLeave);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
 }
